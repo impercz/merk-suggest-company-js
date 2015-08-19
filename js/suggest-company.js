3f1be74009ad1b1eab8b869fea3215d61b09547f
Filip Ambrož <aborym@aborym.cz>
2015-08-19 14:15:40 +0200

    [ADD] Email not replaced

--- js/suggest-company.js
+++ js/suggest-company.js
@@ -19,46 +19,53 @@ $.fn.merkSuggestify = function(suggest_by, proxy_url, api_to_inputs) {
   this.typeahead(null, {
     name: 'companies_result',
     display: function(obj){
-      switch(suggest_by)
-      {
-      case "email":
-        return obj.emails[0]
-        break;
-      default:
-        return obj[suggest_by]
+      switch(suggest_by) {
+        case "email": {
+          return $("input[name='"+api_to_inputs["first_email"]+"']").val();
+          break;
+        }
+        default:
+          return obj[suggest_by]
       }
     },
     source: companies_result,
+
+    remote: {
+        filter: function(parsedResponse) {
+            var dataset = [];
+
+            dataset = parsedResponse.data;
+            console.log(parsedResponse.data);
+            console.log(dataset); // debug the response here
+
+            return dataset;
+        }
+    },
+
     templates: {
-      suggestion: function(obj){ 
+      suggestion: function(obj){ console.log(obj);
         return "<div>" + obj.name + " - " + obj.regno + ", <br>" + obj.address.street + " " + obj.address.number + ", " + obj.address.municipality + "</div>"
       }
     }
   });
-  
-  this.bind('typeahead:select', function(event, object) {      
-     
-    
+  this.bind('typeahead:open', function(event, object) {
+    console.log('OPened', event, object, this)
+  });
+  this.bind('typeahead:select', function(event, object) {
     $("input[name='"+api_to_inputs["name"]+"']").val(object.name);
     $("input[name='"+api_to_inputs["regno"]+"']").val(object.regno);
     $("input[name='"+api_to_inputs["vatno"]+"']").val(object.vatno);
     $("input[name='"+api_to_inputs["first_email"]+"']").val(function(){
-
-      if($(this).val() == "")
+      if($(this).val().replace(/\s/g, '') == '')
         return object.emails[0];
       else
         return $(this).val();
     });;
     $("input[name='"+api_to_inputs["first_phone"]+"']").val(object.phones[0]);
-    
-
-    
     $("input[name='"+api_to_inputs["address"]["municipality"]+"']").val(object.address.municipality);
     $("input[name='"+api_to_inputs["address"]["street"]+"']").val(object.address.street);
     $("input[name='"+api_to_inputs["address"]["number"]+"']").val(object.address.number);
     $("input[name='"+api_to_inputs["address"]["postal_code"]+"']").val(object.address.postal_code);
     $("input[name='"+api_to_inputs["address"]["country_code"]+"']").val(object.address.country_code);
-     
-
   });
 };
